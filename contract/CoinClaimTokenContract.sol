06432aba1461947ca71d9327f2764e8ade46bc6a
Nitin Malik <nitin.malik@ireslab.com>
2018-07-19 20:46:33 +0530

    Updated Smart Contract source code

--- contract/CoinClaimTokenContract.sol
+++ contract/CoinClaimTokenContract.sol
@@ -1,20 +1,16 @@
-pragma solidity ^0.4.21;
+pragma solidity 0.4.21;
 
 
 library SafeMath {
     function mul(uint256 a, uint256 b) internal pure returns (uint256){
-        if (a == 0) {
-            return 0;
-        }
         uint256 c = a * b;
-        assert(c / a == b);
+        assert(a == 0 || c / a == b);
         return c;
     }
 
     function div(uint256 a, uint256 b) internal pure returns (uint256){
-        // assert(b > 0); // Solidity automatically throws when dividing by 0
+        assert(b > 0);
         uint256 c = a / b;
-        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
         return c;
     }
 
@@ -34,17 +30,17 @@ library SafeMath {
 contract ERC20  {
     uint256 public totalSupply;
 
-    function balanceOf(address who) public view returns (uint256);
+    function balanceOf(address who) constant public returns (uint256);
 
     function transfer(address to, uint256 value) public returns (bool);
 
-    event Transfer(address indexed from, address indexed to, uint256 value);
-
-    function allowance(address owner, address spender) public view returns (uint256);
+    function allowance(address owner, address spender) constant public returns (uint256);
 
     function transferFrom(address from, address to, uint256 value) public returns (bool);
 
     function approve(address spender, uint256 value) public returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
     event Approval(address indexed owner, address indexed spender, uint256 value);
 }
@@ -52,42 +48,69 @@ contract ERC20  {
 
 contract StandardToken is ERC20 {
     using SafeMath for uint256;
+    mapping (address => mapping (address => uint256)) allowed;
     mapping(address => uint256) balances;
-    mapping (address => mapping (address => uint256)) internal allowed;
 
+    /**
+     * @dev transfer token for a specified address
+     * @param _to The address to transfer to.
+     * @param _value The amount to be transferred.
+     */
     function transfer(address _to, uint256 _value) public returns (bool){
-        require(_to != address(0));
-        require(_value <= balances[msg.sender]);
+        assert(0 < _value);
+        assert(balances[msg.sender] >= _value);
         // SafeMath.sub will throw if there is not enough balance.
-        
         balances[msg.sender] = balances[msg.sender].sub(_value);
         balances[_to] = balances[_to].add(_value);
         emit Transfer(msg.sender, _to, _value);
         return true;
     }
 
-    function balanceOf(address _owner) public view returns (uint256 balance){
+    /**
+     * @dev Gets the balance of the specified address.
+     * @param _owner The address to query the balance of. 
+     * @return An uint256 representing the amount owned by the passed address.
+     */
+    function balanceOf(address _owner) constant public returns (uint256 balance){
         return balances[_owner];
     }
 
+    /**
+     * @dev Transfer tokens from one address to another
+     * @param _from address The address which you want to send tokens from
+     * @param _to address The address which you want to transfer to
+     * @param _value uint256 the amout of tokens to be transfered
+     */
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool){
-        require(_to != address(0));
-        require(_value <= balances[_from]);
-        require(_value <= allowed[_from][msg.sender]);
-        balances[_from] = balances[_from].sub(_value);
+        uint256 _allowance = allowed[_from][msg.sender];
+        assert (balances[_from] >= _value);
+        assert (_allowance >= _value);
+        assert (_value > 0);
         balances[_to] = balances[_to].add(_value);
-        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+        balances[_from] = balances[_from].sub(_value);
+        allowed[_from][msg.sender] = _allowance.sub(_value);
         emit Transfer(_from, _to, _value);
         return true;
     }
 
+    /**
+     * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
+     * @param _spender The address which will spend the funds.
+     * @param _value The amount of tokens to be spent.
+     */
     function approve(address _spender, uint256 _value) public returns (bool){
         allowed[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
         return true;
     }
 
-    function allowance(address _owner, address _spender) public view returns (uint256){
+    /**
+     * @dev Function to check the amount of tokens that an owner allowed to a spender.
+     * @param _owner address The address which owns the funds.
+     * @param _spender address The address which will spend the funds.
+     * @return A uint256 specifing the amount of tokens still available for the spender.
+     */
+    function allowance(address _owner, address _spender) constant public returns (uint256 remaining){
         return allowed[_owner][_spender];
     }
 }
@@ -117,19 +140,26 @@ contract CoinClaimToken is StandardToken, Ownable{
 
     event Mint(address indexed to, uint256 amount);
 
-    function CoinClaimToken(string _name, string _symbol, uint8 _decimals) public {
+    function CoinClaimToken(string _name, string _symbol, uint8 _decimals) public{
         name = _name;
         symbol = _symbol;
         decimals = _decimals;
         cap = 20000000000000000000000000;
     }
 
-    function mint(address _to, uint256 _amount) public onlyOwner returns (bool){
+    /**
+     * @dev Function to mint tokens
+     * @param _to The address that will recieve the minted tokens.
+     * @param _amount The amount of tokens to mint.
+     * @return A boolean that indicates if the operation was successful.
+     */
+    function mint(address _to, uint256 _amount) onlyOwner public returns (bool){
         assert(maxMintBlock == 0);
         require(totalSupply.add(_amount) <= cap);
         totalSupply = totalSupply.add(_amount);
         balances[_to] = balances[_to].add(_amount);
         emit Mint(_to, _amount);
+        emit Transfer(msg.sender, _to, _amount);
         maxMintBlock = 1;
         return true;
     }
